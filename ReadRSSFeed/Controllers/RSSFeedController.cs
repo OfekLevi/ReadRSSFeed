bdc395f5affa8744c9d4f128f4a716b5ebc2fc34
ofek levi <ofekiko00@gmail.com>
2020-10-14 19:49:05 +0300

    hagasha

--- ReadRSSFeed/Controllers/RSSFeedController.cs
+++ ReadRSSFeed/Controllers/RSSFeedController.cs
@@ -16,26 +16,44 @@ namespace ReadRSSFeed.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Index(string RSSURL)
+        public ActionResult Index(string RSSURL , string CategoryTerm)
         {
             WebClient wc = new WebClient();
             string rss = wc.DownloadString("https://www.nerfplz.com/feeds/posts/default");
             XNamespace atom = "http://www.w3.org/2005/Atom";
             XNamespace media = XNamespace.Get("http://search.yahoo.com/mrss/");
+            XNamespace scheme = XNamespace.Get("http://www.blogger.com/atom/ns#");
             XDocument xml = XDocument.Parse(rss);
 
-            var RSSFeedData = (from q in xml.Descendants(atom + "entry")
-                              select new RSSFeed
-                              {
-                                  Title = q.Element(atom + "title").Value,
-                                  Published = DateTime.Parse(q.Element(atom + "published").Value).ToString("dd/MM/yyyy HH:mm:ss"),
-                                  Url = q.Descendants(atom + "link").Attributes("href").LastOrDefault().Value,
-                                  Image = q.Element(media + "thumbnail") != null ? q.Element(media + "thumbnail").Attribute("url").Value : ""
-                              }).Take(10);
+            var CT = CategoryTerm == "" ? "All Posts" : CategoryTerm;
+
+            var RSSFeedDataAll = from q in xml.Descendants(atom + "entry")                                  
+                                  select new RSSFeed
+                                  {
+                                      Title = q.Element(atom + "title").Value,
+                                      Published = DateTime.Parse(q.Element(atom + "published").Value).ToString("dd/MM/yyyy HH:mm:ss"),
+                                      Url = q.Descendants(atom + "link").Attributes("href").LastOrDefault().Value,
+                                      Image = q.Element(media + "thumbnail") != null ? q.Element(media + "thumbnail").Attribute("url").Value : "",
+                                      Category = q.Element(atom + "category").Attribute("term").Value
+                                  };
+
+            var RSSFeedDataTerm = from q in xml.Descendants(atom + "entry")
+                                  where q.Element(atom + "category").Attribute("term").Value == CT
+                                  select new RSSFeed
+                                  {
+                                      Title = q.Element(atom + "title").Value,
+                                      Published = DateTime.Parse(q.Element(atom + "published").Value).ToString("dd/MM/yyyy HH:mm:ss"),
+                                      Url = q.Descendants(atom + "link").Attributes("href").LastOrDefault().Value,
+                                      Image = q.Element(media + "thumbnail") != null ? q.Element(media + "thumbnail").Attribute("url").Value : "",
+                                      Category = q.Element(atom + "category").Attribute("term").Value
+                                  };
+
+            var RSSFeedData = CategoryTerm == "All Posts" ? RSSFeedDataAll : RSSFeedDataTerm ;
 
             ViewBag.RSSFeed = RSSFeedData;
             ViewBag.URL = RSSURL;
             return View();
         }
+
     }
 }
--- ReadRSSFeed/Models/RSSFeed.cs
+++ ReadRSSFeed/Models/RSSFeed.cs
@@ -11,5 +11,6 @@ namespace ReadRSSFeed.Models
         public string Url { get; set; }
         public string Published { get; set; }        
         public string Image { get; set; }
+        public string Category { get; set; }
     }
 }
